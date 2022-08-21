8e407788ff2f48de2ecc4168da006be435f7bda3
VicSottile <vicksottile@gmail.com>
2022-08-20 21:40:26 -0300

    creada la tabla users y la conexion

--- controllers/contactosController.js
+++ controllers/contactosController.js
@@ -1,14 +1,37 @@
-const knex = require('')
+const knex = require('../db/conexion');
 
-formularioContacto
+const formularioContacto = (req,resp) => {
+    res.render();//renderizar la pagina de contacto
+    res.send(`te envio el formulario de contacto`);
 
-
-
-
-
-selectContacto
+}
 
 
 
 
-crearContacto
+
+const selectContacto = (req,resp) => {
+    knex.select('*')
+    .from ('users')
+    .then((response)=>{
+        for(row of response){
+            console.log(`${nombre} - ${barrio} - ${consulta}`);
+        }
+        //Aqui tendriamos que renderizar
+    });
+    .catch((err)=>{
+        console.log(`${err}`);
+    })
+}
+
+
+
+
+const crearContacto = (req, resp) => {
+   res.send (`Contacto creado`); 
+}
+module.exports={
+    formularioContacto,
+    selectContacto,
+    crearContacto
+}
--- db/conexion.js
+++ db/conexion.js
@@ -1,13 +1,13 @@
 
 
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
-      host : '127.0.0.1',
-      port : 3306,
-      user : 'your_database_user',
-      password : 'your_database_password',
-      database : 'myapp_test'
+        host : '127.0.0.1',
+        port : 3306,
+        user : 'root',
+        password : '123456',
+        database : 'myapp_test'
     }
   });
 
@@ -25,3 +25,5 @@ knex.schema.hasTable('users').then(function(exists) {
       });
     }
   });
+
+  module.exports = knex;
